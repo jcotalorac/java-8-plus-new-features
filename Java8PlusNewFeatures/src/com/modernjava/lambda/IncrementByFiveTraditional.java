27731d2a266539814cd28faf1b382cd31f48d8a3
Juan Carlos Otalora <juan.otalora@globant.com>
2020-08-26 09:55:53 -0500

    Calling traditional interface implementation

--- Java8PlusNewFeatures/src/com/modernjava/lambda/IncrementByFiveTraditional.java
+++ Java8PlusNewFeatures/src/com/modernjava/lambda/IncrementByFiveTraditional.java
@@ -6,4 +6,9 @@ public class IncrementByFiveTraditional implements IncrementByFiveInterface {
     public int incrementByFive(int a) {
         return a + 5;
     }
+
+    public static void main(String[] args) {
+        IncrementByFiveTraditional incrementByFiveTraditional = new IncrementByFiveTraditional();
+        System.out.println(incrementByFiveTraditional.incrementByFive(2));
+    }
 }
