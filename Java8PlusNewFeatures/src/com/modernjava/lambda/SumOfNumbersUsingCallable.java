fd5d10ac6eed1d21d01c6ef04a4d5a955c6ce7b7
Juan Carlos Otalora <juan.otalora@globant.com>
2020-08-27 18:55:28 -0500

    Sending the two callable tasks to the pool of threads for execution and expecting the future results

--- Java8PlusNewFeatures/src/com/modernjava/lambda/SumOfNumbersUsingCallable.java
+++ Java8PlusNewFeatures/src/com/modernjava/lambda/SumOfNumbersUsingCallable.java
@@ -1,13 +1,18 @@
 package com.modernjava.lambda;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.IntStream;
 
 public class SumOfNumbersUsingCallable {
     public static int[] array = IntStream.rangeClosed(0, 5000).toArray();
     public static int total = IntStream.rangeClosed(0, 5000).sum();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Callable callable1 = () -> {
             int sum = 0;
             for (int i = 0; i < array.length; i++) {
@@ -23,5 +28,9 @@ public class SumOfNumbersUsingCallable {
             }
             return sum;
         };
+
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        List<Callable<Integer>> tasks = Arrays.asList(callable1, callable2);
+        List<Future<Integer>> results = executorService.invokeAll(tasks);
     }
 }
