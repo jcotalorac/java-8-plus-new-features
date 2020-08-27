95aab18954b13058933e0d8547ee8104c30755c2
Juan Carlos Otalora <juan.otalora@globant.com>
2020-08-27 18:41:24 -0500

    Callable using lambda expression to enclose the sum job of all elements of the array

--- Java8PlusNewFeatures/src/com/modernjava/lambda/SumOfNumbersUsingCallable.java
+++ Java8PlusNewFeatures/src/com/modernjava/lambda/SumOfNumbersUsingCallable.java
@@ -1,8 +1,19 @@
 package com.modernjava.lambda;
 
+import java.util.concurrent.Callable;
 import java.util.stream.IntStream;
 
 public class SumOfNumbersUsingCallable {
     public static int[] array = IntStream.rangeClosed(0, 5000).toArray();
     public static int total = IntStream.rangeClosed(0, 5000).sum();
+
+    public static void main(String[] args) {
+        Callable callable1 = () -> {
+            int sum = 0;
+            for (int i = 0; i < array.length; i++) {
+                sum += array[i];
+            }
+            return sum;
+        };
+    }
 }
