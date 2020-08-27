dba5a8080091696971e876550bfe768d0c60747a
Juan Carlos Otalora <juan.otalora@globant.com>
2020-08-27 18:26:03 -0500

    Traditional callable class, storing an array from stream of 5000 range numbers

---
+++ Java8PlusNewFeatures/src/com/modernjava/lambda/SumOfNumbersUsingCallable.java
@@ -0,0 +1,7 @@
+package com.modernjava.lambda;
+
+import java.util.stream.IntStream;
+
+public class SumOfNumbersUsingCallable {
+    public static int[] array = IntStream.rangeClosed(0, 5000).toArray();
+}
