a2254bd779a7d90cdc80bb3675b2f3e82b6fee78
Juan Carlos Otalora <juan.otalora@globant.com>
2020-08-27 18:30:05 -0500

    Test value of sum of range declared.

--- Java8PlusNewFeatures/src/com/modernjava/lambda/SumOfNumbersUsingCallable.java
+++ Java8PlusNewFeatures/src/com/modernjava/lambda/SumOfNumbersUsingCallable.java
@@ -4,4 +4,5 @@ import java.util.stream.IntStream;
 
 public class SumOfNumbersUsingCallable {
     public static int[] array = IntStream.rangeClosed(0, 5000).toArray();
+    public static int total = IntStream.rangeClosed(0, 5000).sum();
 }
