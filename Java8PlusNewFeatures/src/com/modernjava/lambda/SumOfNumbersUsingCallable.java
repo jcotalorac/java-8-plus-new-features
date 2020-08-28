60b4edb46fc6d1452c0cae6baea2e30c5591edc3
Juan Carlos Otalora <juan.otalora@globant.com>
2020-08-27 19:13:17 -0500

    Correcting task 1. Summing halfs of the array each apart with its own callable task. Once the results are ready, both ones are summed to contrast with the previous stream total.

--- Java8PlusNewFeatures/src/com/modernjava/lambda/SumOfNumbersUsingCallable.java
+++ Java8PlusNewFeatures/src/com/modernjava/lambda/SumOfNumbersUsingCallable.java
@@ -3,6 +3,7 @@ package com.modernjava.lambda;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -12,10 +13,10 @@ public class SumOfNumbersUsingCallable {
     public static int[] array = IntStream.rangeClosed(0, 5000).toArray();
     public static int total = IntStream.rangeClosed(0, 5000).sum();
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         Callable callable1 = () -> {
             int sum = 0;
-            for (int i = 0; i < array.length; i++) {
+            for (int i = 0; i < array.length / 2; i++) {
                 sum += array[i];
             }
             return sum;
@@ -32,5 +33,17 @@ public class SumOfNumbersUsingCallable {
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         List<Callable<Integer>> tasks = Arrays.asList(callable1, callable2);
         List<Future<Integer>> results = executorService.invokeAll(tasks);
+
+        int k = 0;
+        int sum = 0;
+        for (Future<Integer> result :
+                results) {
+            sum += result.get();
+            System.out.println("Sum of " + ++k + " callable is: " + result.get());
+        }
+
+        System.out.println("Sum from all callables is: " + sum);
+        System.out.println("Correct sum from IntStream is: " + total);
+        executorService.shutdown();
     }
 }
