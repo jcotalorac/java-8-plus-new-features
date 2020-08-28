ed6167ff193c909bd2c1068c4fc2c7f16932ae67
Juan Carlos Otalora <juan.otalora@globant.com>
2020-08-28 18:33:46 -0500

    Examples of use of biconsumer (two arguments)

---
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/BiConsumerExample.java
@@ -0,0 +1,14 @@
+package com.modernjava.funcionalprogramming;
+
+import java.util.function.BiConsumer;
+
+public class BiConsumerExample {
+    public static void main(String[] args) {
+        BiConsumer<Integer, Integer> biConsumer = (x, y) -> System.out.println("x: " + x + ", y: " + y);
+        biConsumer.accept(2, 4);
+        BiConsumer<Integer, Integer> biConsumer1 = (x, y) -> System.out.println("x + y: " + (x + y));
+        biConsumer1.accept(2, 4);
+        BiConsumer<String, String> biConsumer2 = (x, y) -> System.out.println(x + y);
+        biConsumer2.accept("Fell on", " deaf ears");
+    }
+}
