3c9611efd06191601584dfeca30927e894a15ba7
Juan Carlos Otalora <juan.otalora@globant.com>
2020-08-28 18:21:05 -0500

    Specialized long consumer

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/ConsumerExample3.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/ConsumerExample3.java
@@ -1,10 +1,14 @@
 package com.modernjava.funcionalprogramming;
 
 import java.util.function.IntConsumer;
+import java.util.function.LongConsumer;
 
 public class ConsumerExample3 {
     public static void main(String[] args) {
         IntConsumer intConsumer = a -> System.out.println(a * 10);
         intConsumer.accept(10);
+
+        LongConsumer longConsumer = a -> System.out.println(a * 10L);
+        longConsumer.accept(10L);
     }
 }
