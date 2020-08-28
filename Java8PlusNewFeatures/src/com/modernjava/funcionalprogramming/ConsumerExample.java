b236161b28f815da2c3e21f27214ee641f2c3f4e
Juan Carlos Otalora <juan.otalora@globant.com>
2020-08-28 10:37:16 -0500

    Accept is the predefined method to invoke the consumer action

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/ConsumerExample.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/ConsumerExample.java
@@ -5,5 +5,6 @@ import java.util.function.Consumer;
 public class ConsumerExample {
     public static void main(String[] args) {
         Consumer<String> c = x -> System.out.println(x.length() + " the value of x: " + x);
+        c.accept("Up in the air");
     }
 }
