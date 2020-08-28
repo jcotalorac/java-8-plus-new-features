978db7baa2796b822982dd57031ebc80714a92ca
Juan Carlos Otalora <juan.otalora@globant.com>
2020-08-28 10:42:55 -0500

    A consumer using block statement

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/ConsumerExample.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/ConsumerExample.java
@@ -6,5 +6,11 @@ public class ConsumerExample {
     public static void main(String[] args) {
         Consumer<String> c = x -> System.out.println(x.length() + " the value of x: " + x);
         c.accept("Up in the air");
+
+        Consumer<Integer> d = x -> {
+            System.out.println("x * x: " + (x * x));
+            System.out.println("x / x: " + (x / x));
+        };
+        d.accept(10);
     }
 }
