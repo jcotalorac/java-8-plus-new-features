61c12b8fe841c979a7adeab723abea8e9a11e305
Juan Carlos Otalora <juan.otalora@globant.com>
2020-08-28 22:48:33 -0500

    Specialized double predicate

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/PredicateExample3.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/PredicateExample3.java
@@ -1,5 +1,6 @@
 package com.modernjava.funcionalprogramming;
 
+import java.util.function.DoublePredicate;
 import java.util.function.IntPredicate;
 import java.util.function.LongPredicate;
 
@@ -10,5 +11,9 @@ public class PredicateExample3 {
 
         LongPredicate p2 = l -> l > 100L;
         System.out.println(p2.test(11111L));
+
+        DoublePredicate p3 = d -> d < 100.25;
+        DoublePredicate p4 = d -> d > 100.10;
+        System.out.println(p3.and(p4).test(100.15));
     }
 }
