3d99c8757e8c3d1e0c6978558ff9d1d706a5e3e7
Juan Carlos Otalora <juan.otalora@globant.com>
2020-08-28 22:43:47 -0500

    Specialized long predicate

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/PredicateExample3.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/PredicateExample3.java
@@ -1,10 +1,14 @@
 package com.modernjava.funcionalprogramming;
 
 import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
 
 public class PredicateExample3 {
     public static void main(String[] args) {
         IntPredicate p1 = i -> i > 100;
         System.out.println(p1.test(100));
+
+        LongPredicate p2 = l -> l > 100L;
+        System.out.println(p2.test(11111L));
     }
 }
