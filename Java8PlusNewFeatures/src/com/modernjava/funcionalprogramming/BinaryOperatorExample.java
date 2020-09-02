6238fe2949c414ee583645c0ab62fd77cc880bc1
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-02 18:58:22 -0500

    Application of a binary operator. A comparator takes two arguments and returns an output, all of three of the same type

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/BinaryOperatorExample.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/BinaryOperatorExample.java
@@ -1,10 +1,13 @@
 package com.modernjava.funcionalprogramming;
 
+import java.util.Comparator;
 import java.util.function.BinaryOperator;
 
 public class BinaryOperatorExample {
     public static void main(String[] args) {
         BinaryOperator<Integer> binaryOperator = (a, b) -> a + b;
         System.out.println(binaryOperator.apply(2, 4));
+
+        Comparator<Integer> comparator = (a, b) -> a.compareTo(b);
     }
 }
