bea2f4fdb42be3b2e2239d8257c42ced39e8f289
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-02 17:12:25 -0500

    The same work expressed in terms of function

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/UnaryOperatorExample.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/UnaryOperatorExample.java
@@ -1,10 +1,14 @@
 package com.modernjava.funcionalprogramming;
 
+import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 public class UnaryOperatorExample {
     public static void main(String[] args) {
         UnaryOperator<Integer> unary = i -> i * 10;
         System.out.println(unary.apply(100));
+
+        Function<Integer, Integer> function = i -> i * 10;
+        System.out.println(function.apply(100));
     }
 }
