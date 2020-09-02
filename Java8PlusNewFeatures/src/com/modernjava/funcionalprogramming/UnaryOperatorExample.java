3801972b79e7681c75174e739171cb60c5f537fe
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-02 17:36:17 -0500

    Using a double specialized unary operator

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/UnaryOperatorExample.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/UnaryOperatorExample.java
@@ -1,5 +1,6 @@
 package com.modernjava.funcionalprogramming;
 
+import java.util.function.DoubleUnaryOperator;
 import java.util.function.Function;
 import java.util.function.IntUnaryOperator;
 import java.util.function.LongUnaryOperator;
@@ -18,5 +19,8 @@ public class UnaryOperatorExample {
 
         LongUnaryOperator longUnaryOperator = i -> i * 10;
         System.out.println(longUnaryOperator.applyAsLong(100));
+
+        DoubleUnaryOperator doubleUnaryOperator = i -> i * 10.0;
+        System.out.println(doubleUnaryOperator.applyAsDouble(100.0));
     }
 }
