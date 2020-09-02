be718447f70ee60421b0b6e953292ad93af02bb9
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-02 16:00:19 -0500

    Applying a predicate in mapping

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/FunctionExample2.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/FunctionExample2.java
@@ -4,12 +4,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class FunctionExample2 {
     public static void main(String[] args) {
+        Predicate<Instructor> p1 = instructor -> instructor.isOnlineCourses();
         Function<List<Instructor>, Map<String, Integer>> mapFunction = instructors -> {
             Map<String, Integer> map = new HashMap<>();
-            instructors.forEach(instructor -> map.put(instructor.getName(), instructor.getYearsOfExperience()));
+            instructors.forEach(instructor -> {
+                if (p1.test(instructor)) {
+                    map.put(instructor.getName(), instructor.getYearsOfExperience());
+                }
+            });
             return map;
         };
 
