a1ca4463afdbb16b989277562191d3dfa28334f2
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-02 17:17:26 -0500

    Using and integer specialized unary operator

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/UnaryOperatorExample.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/UnaryOperatorExample.java
@@ -1,6 +1,7 @@
 package com.modernjava.funcionalprogramming;
 
 import java.util.function.Function;
+import java.util.function.IntUnaryOperator;
 import java.util.function.UnaryOperator;
 
 public class UnaryOperatorExample {
@@ -10,5 +11,8 @@ public class UnaryOperatorExample {
 
         Function<Integer, Integer> function = i -> i * 10;
         System.out.println(function.apply(100));
+
+        IntUnaryOperator intUnaryOperator = i -> i * 10;
+        System.out.println(intUnaryOperator.applyAsInt(100));
     }
 }
