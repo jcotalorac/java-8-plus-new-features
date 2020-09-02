a55e70d905d025014a5e9f2e39da86b7fb312dcf
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-02 15:51:57 -0500

    Example of mapping function, extracting some attributes to be stored an printed subsequently

---
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/FunctionExample2.java
@@ -0,0 +1,18 @@
+package com.modernjava.funcionalprogramming;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class FunctionExample2 {
+    public static void main(String[] args) {
+        Function<List<Instructor>, Map<String, Integer>> mapFunction = instructors -> {
+            Map<String, Integer> map = new HashMap<>();
+            instructors.forEach(instructor -> map.put(instructor.getName(), instructor.getYearsOfExperience()));
+            return map;
+        };
+
+        System.out.println(mapFunction.apply(Instructors.getAll()));
+    }
+}
