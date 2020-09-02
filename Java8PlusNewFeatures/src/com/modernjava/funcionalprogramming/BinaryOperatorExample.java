5f0d1f5f97f7ac96e48c9ac5e08ea4ed71e04adc
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-02 18:49:09 -0500

    An example of a binary operator, taking 2 arguments and giving an output, all of three of the same type

---
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/BinaryOperatorExample.java
@@ -0,0 +1,10 @@
+package com.modernjava.funcionalprogramming;
+
+import java.util.function.BinaryOperator;
+
+public class BinaryOperatorExample {
+    public static void main(String[] args) {
+        BinaryOperator<Integer> binaryOperator = (a, b) -> a + b;
+        System.out.println(binaryOperator.apply(2, 4));
+    }
+}
