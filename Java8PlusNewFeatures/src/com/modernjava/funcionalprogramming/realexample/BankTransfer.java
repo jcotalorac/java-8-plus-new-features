fafb7415d55af877b1a725005aa4fc9db6fb505c
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-03 17:31:40 -0500

    Bi-predicate to evaluate if balance is enough to transfer an amount

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/realexample/BankTransfer.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/realexample/BankTransfer.java
@@ -1,10 +1,14 @@
 package com.modernjava.funcionalprogramming.realexample;
 
+import java.util.function.BiPredicate;
+
 public class BankTransfer {
     public static void main(String[] args) {
         AccountFactory accountFactory = BankAccount::new;
 
         BankAccount studentBankAccount = accountFactory.getBankAccount(1, 50000, "StudentA");
         BankAccount universityBankAccount = accountFactory.getBankAccount(2, 100000, "University");
+
+        BiPredicate<Double, Double> p1 = (balance, amount) -> balance > amount;
     }
 }
