80471183d1982c7f1826f0f716b97e34b0f051a4
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-03 11:12:07 -0500

    Declaring a lock for transactions between accounts, with reentrant characteristics, allowing transactions to be bound to the current thread

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/realexample/BankAccount.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/realexample/BankAccount.java
@@ -1,7 +1,12 @@
 package com.modernjava.funcionalprogramming.realexample;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
     private int id;
     private double balance;
     private String accountName;
+
+    final Lock lock = new ReentrantLock();
 }
