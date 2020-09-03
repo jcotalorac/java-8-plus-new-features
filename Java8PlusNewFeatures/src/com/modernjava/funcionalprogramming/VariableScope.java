3ee028eedc4f510781affa21cbbb8df48bdfcaf3
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-03 10:23:35 -0500

    In lambda expression is allowed to read local variables

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/VariableScope.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/VariableScope.java
@@ -5,6 +5,6 @@ import java.util.function.IntConsumer;
 public class VariableScope {
     public static void main(String[] args) {
         int a = 10;
-        IntConsumer intConsumer = a -> System.out.println(a * 10);
+        IntConsumer intConsumer = b -> System.out.println(a * 10);
     }
 }
