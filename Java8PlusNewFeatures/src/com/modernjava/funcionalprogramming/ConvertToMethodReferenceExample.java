cc37ad8d9d46abeee88c8e74db8f8be645817035
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-02 22:13:51 -0500

    Using a predicate expressed with method reference

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/ConvertToMethodReferenceExample.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/ConvertToMethodReferenceExample.java
@@ -6,6 +6,12 @@ public class ConvertToMethodReferenceExample {
     public static void main(String[] args) {
         Predicate<Instructor> p2 = instructor -> instructor.getYearsOfExperience() > 10;
         Predicate<Instructor> p3 = ConvertToMethodReferenceExample::greaterThanTenYearsOfExperience;
+
+        Instructors.getAll().forEach(instructor -> {
+            if (p2.test(instructor)) {
+                System.out.println(instructor);
+            }
+        });
     }
 
     public static boolean greaterThanTenYearsOfExperience(Instructor instructor) {
