51d3f2720e787571b726760a11cb09bcd2d3b9b5
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-02 22:35:23 -0500

    Printing results

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/ConstructorReferenceExample.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/ConstructorReferenceExample.java
@@ -7,5 +7,7 @@ public class ConstructorReferenceExample {
         InstructorFactory instructorFactory = Instructor::new;
         Instructor instructor = instructorFactory.getInstructor("Mike", 10, "Software Developer",
                 "M", true, Arrays.asList("Java Programming", "C++ Programming", "Python Programming"));
+
+        System.out.println(instructor);
     }
 }
