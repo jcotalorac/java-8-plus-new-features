e6ea1c3b3360b18a33d09ca262c309c6314c0868
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-03 17:18:08 -0500

    Instancing account factory with constructor reference implementation related with bank account

---
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/realexample/BankTransfer.java
@@ -0,0 +1,7 @@
+package com.modernjava.funcionalprogramming.realexample;
+
+public class BankTransfer {
+    public static void main(String[] args) {
+        AccountFactory accountFactory = BankAccount::new;
+    }
+}
