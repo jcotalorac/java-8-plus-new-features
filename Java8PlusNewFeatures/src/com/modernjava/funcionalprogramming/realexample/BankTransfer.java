ebddf43e852354d26472ca8b52388ceccc918eea
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-03 17:23:32 -0500

    Creating origin and destination bank accounts with account factory interface factory method

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/realexample/BankTransfer.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/realexample/BankTransfer.java
@@ -3,5 +3,8 @@ package com.modernjava.funcionalprogramming.realexample;
 public class BankTransfer {
     public static void main(String[] args) {
         AccountFactory accountFactory = BankAccount::new;
+
+        BankAccount studentBankAccount = accountFactory.getBankAccount(1, 50000, "StudentA");
+        BankAccount universityBankAccount = accountFactory.getBankAccount(2, 100000, "University");
     }
 }
