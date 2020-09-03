35a5dfcbe886c41cb96f09ab040b99dc01c35e3e
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-03 10:29:24 -0500

    Local variable can not be modified inside a lambda block, because it becomes in effectively final variable inside of this block

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/VariableScope.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/VariableScope.java
@@ -1,10 +1,15 @@
 package com.modernjava.funcionalprogramming;
 
+import java.util.List;
 import java.util.function.IntConsumer;
 
 public class VariableScope {
     public static void main(String[] args) {
         int b = 10;
         IntConsumer intConsumer = a -> System.out.println(a * 10);
+
+        int k = 0;
+        List<Instructor> instructors = Instructors.getAll();
+        instructors.forEach(instructor -> System.out.println(instructor + "" + k++));
     }
 }
