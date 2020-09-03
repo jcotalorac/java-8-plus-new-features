20b0152e41ba696d6e7bc8bd1a56eb76e092abf6
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-03 10:35:29 -0500

    For class variables, there are no restrictions to read or modify

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/VariableScope.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/VariableScope.java
@@ -4,13 +4,13 @@ import java.util.List;
 import java.util.function.IntConsumer;
 
 public class VariableScope {
+    static int k = 0;
     public static void main(String[] args) {
         int b = 10;
         IntConsumer intConsumer = a -> System.out.println(a * 10);
 
-        int k = 0;
         List<Instructor> instructors = Instructors.getAll();
-        instructors.forEach(instructor -> System.out.println(instructor + "" + k));
-        k = 6;
+        instructors.forEach(instructor -> System.out.println(instructor + "" + k++));
+        k++;
     }
 }
