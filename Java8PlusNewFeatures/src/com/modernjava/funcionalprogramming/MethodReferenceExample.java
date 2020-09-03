52efb82ad61cb2f8bd7e6c8832b27f9b48faee09
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-02 21:55:38 -0500

    Function with lambda expression and method reference

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/MethodReferenceExample.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/MethodReferenceExample.java
@@ -1,10 +1,14 @@
 package com.modernjava.funcionalprogramming;
 
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class MethodReferenceExample {
     public static void main(String[] args) {
         Predicate<Instructor> p1 = instructor -> instructor.isOnlineCourses();
         Predicate<Instructor> p2 = Instructor::isOnlineCourses;
+
+        Function<Integer, Double> sqrt = n -> Math.sqrt(n);
+        Function<Integer, Double> sqrt2 = Math::sqrt;
     }
 }
