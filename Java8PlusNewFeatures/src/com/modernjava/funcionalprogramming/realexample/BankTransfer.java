66b948490f033246a58c67654db1de69debb1b15
Juan Carlos Otalora <juan.otalora@globant.com>
2020-09-03 20:18:23 -0500

    Printing final balances with functional bi-consummer

--- Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/realexample/BankTransfer.java
+++ Java8PlusNewFeatures/src/com/modernjava/funcionalprogramming/realexample/BankTransfer.java
@@ -51,5 +51,7 @@ public class BankTransfer {
         } catch (InterruptedException i) {
             i.printStackTrace();
         }
+
+        printer2.accept(studentBankAccount, universityBankAccount);
     }
 }
